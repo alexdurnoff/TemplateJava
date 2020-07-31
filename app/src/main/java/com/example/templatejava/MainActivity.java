b90b3d88fb3d1479725fd810a19c54bb71eae929
Alexej <door1975@yandex.ru>
2020-07-31 13:12:37 +0300

    Переписал метод onCreate. Там вставляется таблица в ScrollView. Пока не тестировал. Сейчас буду разбираться с kvm.

--- app/src/main/java/com/example/templatejava/MainActivity.java
+++ app/src/main/java/com/example/templatejava/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.templatejava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ScrollView scrollView = findViewById(R.id.my_scroll);
+        TableLayout tableLayout = new TableLayout(this);
+        for (int i = 0; i < 1000; i++){
+            TableRow tableRow = new TableRow(this);
+            for (int j = 0; j <25; j++){
+                TextView textView = new TextView(this);
+                tableRow.addView(textView);
+            }
+            tableLayout.addView(tableRow);
+        }
+        scrollView.addView(tableLayout);
     }
 }
