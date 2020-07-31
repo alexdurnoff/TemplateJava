e526caccd655e7ee77679914b408ec51d7f4c55f
Alexej <door1975@yandex.ru>
2020-07-31 22:37:59 +0300

    Под конец дня заработал самый простой вариант, когда таблица создается программно. Двадцать строчек, в которых по двадцать элементов editText. Сейчас увеличу количество строк, и на сегодня все.

--- app/src/main/java/com/example/templatejava/MainActivity.java
+++ app/src/main/java/com/example/templatejava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.templatejava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -14,13 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ScrollView scrollView = findViewById(R.id.my_scroll);
+        ScrollView scrollView = findViewById(R.id.scroll);
         TableLayout tableLayout = new TableLayout(this);
-        for (int i = 0; i < 1000; i++){
+        for (int i = 0; i < 20; i++){
             TableRow tableRow = new TableRow(this);
-            for (int j = 0; j <25; j++){
-                TextView textView = new TextView(this);
-                tableRow.addView(textView);
+            for (int j = 0; j <20; j++){
+                EditText testText = new EditText(this);
+                testText.setText("говно");
+                testText.setWidth(100);
+                tableRow.addView(testText);
             }
             tableLayout.addView(tableRow);
         }
