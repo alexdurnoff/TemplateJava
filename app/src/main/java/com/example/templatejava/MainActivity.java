117fa71ebd15a23bd99824edf17a7d16c79b2c38
Alexej <door1975@yandex.ru>
2020-08-18 21:38:39 +0300

    Перешел на GridLayoutManager для RecyclerView. Теперь получается таблица. Это хорошо. На в таблице везде отображаются только значения петли. Значит, нужно читать в gridlayout из одномерного массива. Что-то типа такого сейчас попробую изобразить.

--- app/src/main/java/com/example/templatejava/MainActivity.java
+++ app/src/main/java/com/example/templatejava/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.templatejava;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.io.FileInputStream;
+import java.util.logging.*;
+
 
 public class MainActivity extends AppCompatActivity {
+
     private RecyclerView recyclerView;
     private DataAdapter myAdapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -18,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.recyclerView = findViewById(R.id.spreadsheet);
         recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
+        layoutManager = new GridLayoutManager(this, 19);
         recyclerView.setLayoutManager(layoutManager);
         DataLine[] dataLines = new DataLine[50];
         for (int i = 0; i <50; i++){
@@ -45,15 +50,8 @@ public class MainActivity extends AppCompatActivity {
             DataLine dataLine = new DataLine(source_data, i);
             dataLines[i] = dataLine;
         }
-        System.out.println("Создали массив данных");
-        this.layoutManager = new LinearLayoutManager(this);
-        System.out.println("Создали layoutManager");
-        this.recyclerView.setLayoutManager(layoutManager);
-        System.out.println("Добавили LayoutManager к RecyclerView");
         this.myAdapter = new DataAdapter(dataLines);
-        System.out.println("Создали адаптер");
         this.recyclerView.setAdapter(this.myAdapter);
-        System.out.println("Добавили адаптер в RecyclerView");
     }
 
 }
